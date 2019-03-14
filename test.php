29a87bc169787991cea2371539701c76eb7f79b4
hambokka <hambokka21@naver.com>
2019-03-14 15:15:18 +0900

    Snow 테스트

--- test.php
+++ test.php
@@ -5,9 +5,9 @@
  * Date: 2019-03-14
  * Time: 오후 1:59
  */
-echo 'don\'t bother';
+    echo 'don\'t bother';
     while( $I <= 10)
     {
-        echo "$I<br>";
+        echo "&nbsp&nbsp&nbsp&nbsp$I<br>";
         $I++;
     }
